3c6f2878dfe4ef5199c6f044f9ccee4b95fd1d6d
Skolzack <estevesrj@hotmail.com>
2017-07-19 17:24:55 -0300

    Update IIS.Importing.Teste.ps1

--- IIS.Importing.Teste.ps1
+++ IIS.Importing.Teste.ps1
@@ -4,6 +4,6 @@ Disable-UAC
 
 $importsite = "https://raw.githubusercontent.com/nibodev/machineSetup/master/sites.xml"
 #$wallpaperFile = "$env:USERPROFILE\Nibo-Wallpaper-1366x768.png"
-Invoke-WebRequest $importsite -OutFile "C:\sites.xml"
+Invoke-WebRequest $importsite -OutFile "C:\Windows\system32\sites.xml"
 
 gc .\sites.xml | C:\Windows\System32\inetsrv\appcmd.exe add site /in
