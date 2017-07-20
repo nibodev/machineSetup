b35fa7f7dd9aa71657e134c9ad4db655a0afc378
Skolzack <estevesrj@hotmail.com>
2017-07-20 11:05:38 -0300

    Update IIS.Importing.Teste.ps1

--- IIS.Importing.Teste.ps1
+++ IIS.Importing.Teste.ps1
@@ -4,8 +4,10 @@ Disable-UAC
 
 $importsite = "https://raw.githubusercontent.com/nibodev/machineSetup/master/sites.xml"
 $importpools = "https://raw.githubusercontent.com/nibodev/machineSetup/master/apppools.xml"
-Invoke-WebRequest $importsite -OutFile "C:\Windows\system32\sites.xml"
-Invoke-WebRequest $importpools -OutFile "C:\Windows\system32\apppools.xml"
+$sites = "$env:USERPROFILE\sites.xml"
+$pools = "$env:USERPROFILE\apppools.xml"
+Invoke-WebRequest $importsite -OutFile $sites
+Invoke-WebRequest $importpools -OutFile $pools
 
 gc .\sites.xml | C:\Windows\System32\inetsrv\appcmd.exe add site /in
 
