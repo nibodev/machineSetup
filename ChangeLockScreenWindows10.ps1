e14b7aacf1b8f9a3fe204e7df64b32a1c59aff54
Skolzack <estevesrj@hotmail.com>
2017-07-21 14:36:52 -0300

    Update ChangeLockScreenWindows10.ps1

--- ChangeLockScreenWindows10.ps1
+++ ChangeLockScreenWindows10.ps1
@@ -12,5 +12,5 @@ Remove-Item -Path "$env:programdata\Microsoft\Windows\SystemData\S-1-5-18\ReadOn
 Start-Process -filePath "$env:systemroot\system32\takeown.exe" -ArgumentList "/F `"$env:systemroot\Web\Screen`" /R /A /D Y" -NoNewWindow -Wait
 Start-Process -filePath "$env:systemroot\system32\icacls.exe" -ArgumentList "`"$env:systemroot\Web\Screen`" /grant Administrators:(OI)(CI)F /T" -NoNewWindow -Wait
 Start-Process -filePath "$env:systemroot\system32\icacls.exe" -ArgumentList "`"$env:systemroot\Web\Screen`" /reset /T" -NoNewWindow -Wait
-Copy-Item -Path "$env:systemroot\Web\Screen\img100.jpg" -Destination "$env:systemroot\Web\Screen\img200.jpg" -Force
-Copy-Item -Path "$wallpaperfile" -Destination "$env:systemroot\Web\Screen\img100.jpg" -Force
+Copy-Item -Path "$env:systemroot\Web\Screen\Nibo-Wallpaper-1366x768.png" -Destination "$env:systemroot\Web\Screen\img200.jpg" -Force
+Copy-Item -Path "$wallpaperfile" -Destination "$env:systemroot\Web\Screen\Nibo-Wallpaper-1366x768.png" -Force
