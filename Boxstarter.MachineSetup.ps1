457093253c5e3d2f75331e9e40819054e7228476
Skolzack <estevesrj@hotmail.com>
2017-07-21 13:57:37 -0300

    Adicionando função de clonar os repositórios.

--- Boxstarter.MachineSetup.ps1
+++ Boxstarter.MachineSetup.ps1
@@ -150,6 +150,42 @@ gc .\sites.xml | C:\Windows\System32\inetsrv\appcmd.exe add site /in
 gc .\apppools.xml | C:\Windows\System32\inetsrv\appcmd.exe add apppool /in
 ###################################################################################################
 
+############################ Clonando repositorios #####################################################################################
+mkdir C:\Git
+cd C:\Git
+
+$url = "https://nibogestao.visualstudio.com/DefaultCollection/NiboProjects"
+$username = Read-host -Prompt "Digite seu email:" #exemplo: xxxx@nibo.com.br
+$password = Read-host -Prompt "Digite o seu token para baixar os repositorios:" #precisa do token de acesso dos repositorios do visual studio
+
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+$headers = @{
+    "Authorization" = ("Basic {0}" -f $base64AuthInfo)
+    "Accept" = "application/json"
+}
+
+Add-Type -AssemblyName System.Web
+$gitcred = ("{0}:{1}" -f  [System.Web.HttpUtility]::UrlEncode($username),$password)
+
+$resp = Invoke-WebRequest -Headers $headers -Uri ("{0}/_apis/git/repositories?api-version=1.0" -f $url)
+$json = convertFrom-JSON $resp.Content
+
+$initpath = get-location
+foreach ($entry in $json.value) { 
+    $name = $entry.name 
+    Write-Host $name
+
+    $url = $entry.remoteUrl -replace "://", ("://{0}@" -f $gitcred)
+    if(!(Test-Path -Path $name)) {
+        git clone $url
+    } else {
+        set-location $name
+        git pull
+        set-location $initpath
+    }
+}
+#######################################################################################################################################
+
 Enable-UAC
 
 if (Test-PendingReboot) { Invoke-Reboot }
