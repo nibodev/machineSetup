aca4cedca9db1dde072f82062995917f7b97ac57
Skolzack <estevesrj@hotmail.com>
2017-08-03 13:17:46 -0300

    Update IIS.Importing.Teste.ps1

--- IIS.Importing.Teste.ps1
+++ IIS.Importing.Teste.ps1
@@ -2,13 +2,14 @@ Update-ExecutionPolicy Unrestricted
 
 Disable-UAC
 
+############# Importando os XML's dos Sites e AppPools pro IIS #####################################
 $importsite = "https://raw.githubusercontent.com/nibodev/machineSetup/master/sites.xml"
 $importpools = "https://raw.githubusercontent.com/nibodev/machineSetup/master/apppools.xml"
 $sites = "$env:USERPROFILE\sites.xml"
 $pools = "$env:USERPROFILE\apppools.xml"
 Invoke-WebRequest $importsite -OutFile $sites
 Invoke-WebRequest $importpools -OutFile $pools
+gc $sites | C:\Windows\System32\inetsrv\appcmd.exe add site /in
+gc $pools | C:\Windows\System32\inetsrv\appcmd.exe add apppool /in
+####################################################################################################
 
-gc .\sites.xml | C:\Windows\System32\inetsrv\appcmd.exe add site /in
-
-gc .\apppools.xml | C:\Windows\System32\inetsrv\appcmd.exe add apppool /in
