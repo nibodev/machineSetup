85949db8ee15ec5faf4cc5431630e693d13e433f
Skolzack <estevesrj@hotmail.com>
2017-08-03 13:16:56 -0300

    Delete StarterScript.ps1

--- StarterScript.ps1
+++
@@ -1,7 +0,0 @@
-Set-ExecutionPolicy Unrestricted
-
-. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
-
-refreshenv.cmd
-
-Install-BoxstarterPackage -PackageName http://bit.ly/2u5T5vw
